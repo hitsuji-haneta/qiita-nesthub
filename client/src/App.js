4d77755e5c919f35dd0b9f51589548efe957f5e8
hitsuji-haneta <hitsuji-haneta@hotmail.com>
2019-10-20 02:20:48 +0900

    Get a tag list

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import styled from 'styled-components';
 
-function App() {
+const Container = styled.div`
+  font-color: #282c34;
+  background-color: #55C501;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(14px + 2vmin);
+  color: white;
+`;
+
+const Button = styled.button`
+  width: 100px;
+  height: 70px;
+  border-radius: 20px;
+  font-size: 1em;
+`;
+
+const fetchTags = async setTags => {
+  const uri = `https://qiita.com/api/v2/users/hitsuji-haneta/following_tags`;
+  const res = await fetch(uri, {
+    headers: {
+      Authorization: 'Bearer ba345cb906f722da9a1300d31a47c7434be7a5d8'
+    }
+  });
+  const resJson = await res.json();
+  console.log(resJson);
+  setTags(resJson);
+};
+
+const TagList = ({ tags }) => tags.map(elm => <p>{elm.id}</p>);
+
+const App = () => {
+  const [tags, setTags] = useState([]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <TagList tags={tags} />
+      <Button onClick={() => fetchTags(setTags)}>qiita</Button>
+    </Container>
   );
 }
 
